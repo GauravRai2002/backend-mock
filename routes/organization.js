12ae73737053303a42be20d9e14c37f5a371546d
Gaurav <gauravrai2002@gmail.com>
2025-07-13 07:33:45 +0530

    organization apis intermediate

--- routes/organization.js
+++ routes/organization.js
@@ -1,8 +1,39 @@
 const express = require("express");
 const router = express.Router();
+const turso = require("../db");
 
-router.get("/", (req, res) => {
-  res.status(200).send("Hello World");
+router.get("/get-all-organizations", async(req, res) => {
+    try{
+        const organizations = await turso.execute("SELECT * FROM organization");
+        res.status(200).send(organizations);
+    }catch(error){
+        console.error("Error fetching organizations:", error);
+        res.status(500).json({ error: "Failed to fetch organizations" });
+    }
+});
+
+router.post("create-organization/:id", async(req, res) => {
+    try{
+        const id = req.params.id;
+        const { organizationName, createdAt, createdBy } = req.body;
+        const createOrganization = await turso.execute("INSERT INTO organization (organization_id, organization_name, created_at, created_by) values (?, ?, ?, ?)", [id, organizationName, createdAt, createdBy]);
+        res.status(200).send(createOrganization);
+    }catch(error){
+        console.error("Error creating organization:", error);
+        res.status(500).json({ error: "Failed to create organization" });
+    }
+});
+
+
+router.get("/get-organization-by-id/:id", async(req, res) => {
+    try{
+        const id = req.params.id;
+        const organization = await turso.execute("SELECT * FROM organization WHERE organization_id = ?", [id]);
+        res.status(200).send(organization);
+    }catch(error){
+        console.error("Error fetching organization:", error);
+        res.status(500).json({ error: "Failed to fetch organization" });
+    }
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -18,13 +18,39 @@ router.get("/", async (req, res) => {
 
 
 router.post("/:id", async (req, res) => {
-  const id = req.params.id;
-  const { userName, firstName, lastName, email, createdAt } = req.body;
-  const user = await turso.execute(
-    "INSERT INTO users (user_id, user_name, first_name, last_name, email, created_at) VALUES (?, ?, ?, ?, ?, ?)",
-    [id, userName, firstName, lastName, email, createdAt]
-  );
-  res.status(201).json(user);
+    try{
+        const id = req.params.id;
+        const { userName, firstName, lastName, email, createdAt } = req.body;
+        const user = await turso.execute(
+          "INSERT INTO users (user_id, user_name, first_name, last_name, email, created_at) VALUES (?, ?, ?, ?, ?, ?)",
+          [id, userName, firstName, lastName, email, createdAt]
+        );
+        res.status(201).json(user);
+    }catch(error){
+        console.error("Error creating user:", error);
+        res.status(500).json({ error: "Failed to create user" });
+    }
+});
+
+router.get("/get-organizations/:userId", async(req, res)=>{
+  try{
+    const { userId } = req.params;
+    const userOrganizations = turso.execute("SELECT * from members where user_id = ?",[userId]);
+    res.status(200).send(userOrganizations)
+  }catch(error){
+    console.error(error);
+    res.status(500).json({ error : error})
+  }
+});
+
+
+router.post("/add-user-to-organization/:userId", async(req, res)=>{
+  try{
+
+  }catch(error){
+    console.error(error);
+    res.status(500).json({message:'cound not add user to the organization', error:error});
+  }
 });
 
 module.exports = router;
